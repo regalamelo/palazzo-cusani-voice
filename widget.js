02e40c437f0bf562c0c4a723347de089e84c5c76
regalamelo <andreacavallaro.it@gmail.com>
2026-04-30 08:18:06 +0200

    Update widget.js

--- widget.js
+++ widget.js
@@ -18,7 +18,7 @@ const SCRIPT_URL = document.currentScript?.src || window.location.href;
 const BASE_URL = new URL("./", SCRIPT_URL);
 const SESSION_URL = new URL("api/session", BASE_URL).toString();
 const LEAD_URL = new URL("api/lead", BASE_URL).toString();
-const BUTTON_IMAGE_URL = new URL("adriana-voice.svg?v=2", BASE_URL).toString();
+const BUTTON_IMAGE_URL = new URL("adriana-voice.svg?v=3", BASE_URL).toString();
 
 const notes = [];
 const lead = {
